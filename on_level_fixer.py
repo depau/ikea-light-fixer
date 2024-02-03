6c85086c54154cef6cc78a98a3406c8837b6e457
Davide Depau <davide@depau.eu>
2024-02-03 15:46:49 +0100

    Rate-limit setting on-level

--- on_level_fixer.py
+++ on_level_fixer.py
@@ -4,29 +4,47 @@ import asyncio
 import json
 import os
 import traceback
-from typing import cast
+from typing import cast, Optional
 
 import aiomqtt
+
+DECIDE_AFTER = float(os.environ.get("DECIDE_AFTER", "1"))  # seconds
 
 
 class LightFixer:
     light_name: str
     client: aiomqtt.Client
+    last_on_level: Optional[str]
+    set_on_level_task: Optional[asyncio.Task]
+    decide_at: float
 
     def __init__(self, light_name: str, client: aiomqtt.Client):
         self.light_name = light_name
         self.client = client
         self.loop = asyncio.get_running_loop()
+        self.last_on_level = None
+        self.task = None
+        self.loop = asyncio.get_running_loop()
+        self.decide_at = 0
 
     @staticmethod
     def is_light(json_message: dict) -> bool:
-        return "level_config" in json_message and "on_level" in json_message["level_config"]
-
-    async def set_on_level(self):
-        await self.client.publish(
-            f"zigbee2mqtt/{self.light_name}/1/set",
-            payload='{"write":{"cluster":"genLevelCtrl","options":{},"payload":{"onLevel":255}}}',
+        return (
+            "level_config" in json_message
+            and "on_level" in json_message["level_config"]
         )
+
+    async def set_on_level_task(self):
+        while self.loop.time() < self.decide_at:
+            await asyncio.sleep(self.decide_at - self.loop.time())
+        self.task = None
+
+        if self.last_on_level != "previous":
+            await self.client.publish(
+                f"zigbee2mqtt/{self.light_name}/1/set",
+                payload='{"write":{"cluster":"genLevelCtrl","options":{},"payload":{"onLevel":255}}}',
+            )
+            print(f"Reset onLevel of {self.light_name} to 'previous'")
 
     async def handle_message(self, json_payload: dict):
         if "level_config" not in json_payload:
@@ -36,11 +54,13 @@ class LightFixer:
         if "on_level" not in level_config:
             return
 
-        on_level = level_config["on_level"]
+        self.last_on_level = level_config["on_level"]
 
-        if on_level != "previous":
-            await self.set_on_level()
-            print(f"Reset onLevel of {self.light_name} to 'previous'")
+        now = self.loop.time()
+        self.decide_at = now + DECIDE_AFTER
+
+        if self.task is None:
+            self.task = self.loop.create_task(self.set_on_level_task())
 
 
 async def main():
@@ -68,7 +88,7 @@ async def main():
                     if name not in lights:
                         lights[name] = LightFixer(name, client)
                         print(f"Learned about new light {name}, setting onLevel")
-                        await lights[name].set_on_level()
+                        await lights[name].set_on_level_task()
 
                     await lights[name].handle_message(j)
 
